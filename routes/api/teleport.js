0137fd684923cd0f287178cecd5cc525cb9be5b2
CheckShumm <checkshumm@gmail.com>
2018-08-07 17:46:17 -0400

    teleport api, city validation

--- routes/api/teleport.js
+++ routes/api/teleport.js
@@ -37,10 +37,10 @@ router.get("/test", (req, res) => {
 	convertJSON(processRequest, teleport, res);
 });
 
-// @route   GET api/teleport/city/:city_name/urban/:ua_id
-// @desc    get city details from ua_id
+// @route   GET api/teleport/city/:city_name/urban/:ua_id/scores
+// @desc    get city scores from ua_id
 // @access  Public route
-router.get("/city/:city_name/urban_area/:ua_id", (req, res) => {
+router.get("/city/:city_name/urban_area/:ua_id/scores", (req, res) => {
 	const scoresURL =
 		teleport + "/urban_areas/slug:" + req.params.city_name + "/scores";
 
@@ -49,6 +49,48 @@ router.get("/city/:city_name/urban_area/:ua_id", (req, res) => {
 	//  teleport + "/urban_areas/teleport%3A" + req.params.ua_id + "/scores";
 
 	convertJSON(processRequest, scoresURL, res);
+});
+
+// @route   GET api/teleport/city/:city_name/urban/:ua_id/salaries
+// @desc    get city details from ua_id
+// @access  Public route
+router.get("/city/:city_name/urban_area/:ua_id/salaries", (req, res) => {
+	const salariesURL =
+		teleport + "/urban_areas/slug:" + req.params.city_name + "/salaries";
+
+	// url using ua_id
+	// 	const salariesURL =
+	//  teleport + "/urban_areas/teleport%3A" + req.params.ua_id + "/salaries";
+
+	convertJSON(processRequest, salariesURL, res);
+});
+
+// @route   GET api/teleport/city/:city_name/urban/:ua_id/details
+// @desc    get city details from ua_id
+// @access  Public route
+router.get("/city/:city_name/urban_area/:ua_id/details", (req, res) => {
+	const detailsURL =
+		teleport + "/urban_areas/slug:" + req.params.city_name + "/details";
+
+	// url using ua_id
+	// 	const detailsURL =
+	//  teleport + "/urban_areas/teleport%3A" + req.params.ua_id + "/details";
+
+	convertJSON(processRequest, detailsURL, res);
+});
+
+// @route   GET api/teleport/city/:city_name/urban/:ua_id/images
+// @desc    get city images from ua_id
+// @access  Public route
+router.get("/city/:city_name/urban_area/:ua_id/images", (req, res) => {
+	const imagesURL =
+		teleport + "/urban_areas/slug:" + req.params.city_name + "/images";
+
+	// url using ua_id
+	// 	const imagesURL =
+	//  teleport + "/urban_areas/teleport%3A" + req.params.ua_id + "/images";
+
+	convertJSON(processRequest, imagesURL, res);
 });
 
 module.exports = router;
